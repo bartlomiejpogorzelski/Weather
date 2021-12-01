3d4f49b05e24b7a4bb184904b1e40898d2974b4d
DESKTOP-38S6RQS\Admin <bartlomiej.pogorzelski@onet.pl>
2021-12-01 22:07:35 +0100

    Added additional functionalities

--- src/main/java/weatherApp/controller/WeatherController.java
+++ src/main/java/weatherApp/controller/WeatherController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import javax.xml.ws.RequestWrapper;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
@@ -19,15 +20,17 @@ public class WeatherController {
 	private OkHttpClient client;
 	private Response response;
 	private String city;
+	private String unit;
 	
-	private JSONObject getWeather(){
+	private JSONObject getWeather() throws JSONException{
 		client = new OkHttpClient();
 		
 		Request request = new Request
-				.Builder().url("http://api.openweathermap.org/data/2.5/weather?q=Londyn&appid=908e83ada5d6312d5bb8a40033d8ea3a")
+				.Builder() .url("http://api.openweathermap.org/data/2.5/weather?q="+getCity()+"&units="+getUnit()+"&appid=908e83ada5d6312d5bb8a40033d8ea3a")
 				.build();
 		try {
 			response = client.newCall(request).execute();
+		    return new JSONObject(response.body().string());
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -42,5 +45,47 @@ public class WeatherController {
 	}
     return jo;
 	}
+
+	public JSONArray getWeatherArray() throws JSONException{
+		JSONArray weatherArray = getWeather().getJSONArray("weather");
+		return weatherArray;
+		
+	}
+	
+	public JSONObject getMainObject() throws JSONException {
+		JSONObject main = getWeather().getJSONObject("main");
+		return main;
+	}
+  
+	public JSONObject getWindObject() throws JSONException {
+        JSONObject wind = getWeather().getJSONObject("wind");
+        return wind;
+        }
+    public JSONObject getSysObject() throws JSONException{
+        JSONObject sys = getWeather().getJSONObject("sys");
+        return sys;
+        } 
+
+
+	//public JSONObject get
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	
+	
+	
+	
 	
 }
--- src/main/java/weatherApp/view/MainView.java
+++ src/main/java/weatherApp/view/MainView.java
@@ -2,7 +2,12 @@ package weatherApp.view;
 
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.parsing.Location;
+import org.springframework.stereotype.Component;
 
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.ClassResource;
@@ -10,18 +15,43 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.NativeSelect;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
+import weatherApp.controller.WeatherController;
+
+
 @SpringUI(path = "")
 public class MainView extends UI {
 	private VerticalLayout mainLayout;
+	private Button searchButton ;
+	private TextField cityText;
+	@Autowired
+	private WeatherController weatherController;
+	private NativeSelect<String> unitSelect ;
+	private Label location;
+	private Label weatherMin;
+    private Label weatherMax;
+	private HorizontalLayout mainDescriptionLayout;
+	private Label weatherDescription;
+	private Label pressureLabel;
+	private Label humidityLabel;
+	private Label windSpeedLabel;
+	private Label feelsLike;
+	private HorizontalLayout dashboardLayout;
+	private VerticalLayout descriptionLayout ;
+	private HorizontalLayout footer;
+	private Label currentTemp;
+	
+	
 	@Override
 	protected void init(VaadinRequest request) {
 		mainLayout();
@@ -29,13 +59,30 @@ public class MainView extends UI {
 		setLogo();
 		setForm();
 		dashboardTitle();
+		dashboardDetails();
+		
+		footer();
+		searchButton.addClickListener(clickEvent -> {
+	           if (!cityText.getValue().equals("")){
+	               try {
+	            	 updateScreen();
+	               } catch (JSONException e) {
+	                   e.printStackTrace();
+	               }
+	           }else
+	               Notification.show("Please Enter The City");
+	        });
 		
 	}
 	
 	private void mainLayout() {
 		mainLayout = new VerticalLayout();
 		mainLayout.setMargin(true);
-		mainLayout.setSizeFull();
+		//mainLayout.setSizeFull();
+		mainLayout.setWidth("100%");
+	    mainLayout.setSpacing(true);
+		mainLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		mainLayout.setStyleName("BackColorGrey");
 		setContent(mainLayout);
 	}
 	
@@ -44,8 +91,8 @@ public class MainView extends UI {
 		header.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
 		Label title = new Label("Simple Weather APP");
 		title.addStyleName(ValoTheme.LABEL_BOLD);
-		header.addComponent(title);
 		
+		header.addComponent(title);		
 		mainLayout.addComponents(header);
 	}
 	
@@ -67,7 +114,7 @@ public class MainView extends UI {
 		form.setMargin(true);
 		form.setSpacing(true);
 		
-		NativeSelect<String> unitSelect = new NativeSelect<>();
+		unitSelect = new NativeSelect<>();
 		unitSelect.setHeight("60px");
 		ArrayList<String> items = new ArrayList<>();
 		items.add("C");
@@ -76,11 +123,11 @@ public class MainView extends UI {
 		unitSelect.setItems(items);
 		unitSelect.setValue(items.get(0));
 		
-		TextField cityText = new TextField();
+		cityText = new TextField();
 		cityText.setWidth("70px");
 		
 		
-		Button searchButton = new Button();
+		searchButton = new Button();
 		searchButton.setIcon(VaadinIcons.SEARCH);
 		
 		
@@ -92,18 +139,122 @@ public class MainView extends UI {
 	}
 	
 	private void dashboardTitle() {
-		HorizontalLayout dashboardLayout = new HorizontalLayout();
+		dashboardLayout = new HorizontalLayout();
 		dashboardLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
 		
-		Label location = new Label("Place to show: ");
+		location = new Label("City to choose");
 		location.addStyleName(ValoTheme.LABEL_H2);
         location.addStyleName(ValoTheme.LABEL_LIGHT);
         
-        Label currentTemp = new Label("20C");
+        currentTemp = new Label("---");
         currentTemp.setStyleName(ValoTheme.LABEL_BOLD);
         currentTemp.setStyleName(ValoTheme.LABEL_H1);
         
         dashboardLayout.addComponents(location,currentTemp);
         mainLayout.addComponents(dashboardLayout);
 	}
+	
+	 private void dashboardDetails(){
+	        mainDescriptionLayout = new HorizontalLayout();
+	        mainDescriptionLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+	        descriptionLayout = new VerticalLayout();
+	        descriptionLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+	        
+	        weatherDescription = new Label("Description: Clear Skies");
+	        weatherDescription.setStyleName(ValoTheme.LABEL_SUCCESS);
+	        descriptionLayout.addComponents(weatherDescription);
+
+	       
+	        weatherMin = new Label("Min tempreture:10");
+	        descriptionLayout.addComponents(weatherMin);
+	     
+	        weatherMax = new Label("Max temp:22");
+	        descriptionLayout.addComponents(weatherMax);
+
+	        // Pressure, humidity, wind, 
+
+	        VerticalLayout pressureLayout = new VerticalLayout();
+	        pressureLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+	        pressureLabel = new Label("Pressure:123Pa");
+	        pressureLayout.addComponents(pressureLabel);
+
+//	        humidityLabel = new Label("Humidity:34");
+//	        pressureLayout.addComponents(humidityLabel);
+
+	        windSpeedLabel = new Label("124/hr");
+	        pressureLayout.addComponents(windSpeedLabel);
+
+//	        feelsLike = new Label("FeelsLike:");
+//	        pressureLayout.addComponents(feelsLike);
+
+
+
+
+	        mainDescriptionLayout.addComponents(descriptionLayout, pressureLayout);
+
+	    }
+	 
+	    private void footer(){
+	        footer = new HorizontalLayout();
+	        footer.setDefaultComponentAlignment(Alignment.BOTTOM_CENTER);
+	        footer.setSpacing(true);
+	        footer.setMargin(true);
+	        footer.setWidth("100%");
+	        footer.setHeight("40px");
+	        Label description = new Label();
+	        description.setValue("Weather App by BP ");
+	        footer.addComponents(description);
+	        mainLayout.addComponents(footer);
+	    }
+	    
+	private void updateScreen() throws JSONException {
+		
+		
+		String city = cityText.getValue();
+		
+		try{
+			weatherController.setCity(city);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		
+		location.setValue("Weather in the city: "+city);
+		
+		
+		if(unitSelect.getValue().equals("C")){
+			weatherController.setUnit("metric");
+		}else {
+			weatherController.setUnit("imperials");
+		}		
+		JSONObject mainObject = weatherController.getMainObject();
+	    int temp = mainObject.getInt("temp");
+	    currentTemp.setValue(temp+ unitSelect.getValue());
+	    
+	    String weatherDescriptionNew = null;
+	    JSONArray jsonA = weatherController.getWeatherArray();
+	    for(int i=0;i<jsonA.length(); i++ ){
+	    	weatherDescriptionNew = jsonA.getJSONObject(i).getString("description"); 
+	    }
+	    
+	    
+		weatherMin.setValue("min temp: " + weatherController.getMainObject().getInt("temp_min")+ unitSelect.getValue());
+		weatherMax.setValue("max temp: " + weatherController.getMainObject().getInt("temp_max")+ unitSelect.getValue() );		
+		
+		pressureLabel.setValue("Pressure: "+weatherController.getMainObject().getInt("pressure"));
+	     
+		weatherDescription.setValue("Description "+ weatherDescriptionNew);
+	  //  humidityLabel.setValue("Humidity: "+weatherController.getMainObject().getInt("humidity"));
+	   windSpeedLabel.setValue("Wind: "+weatherController.getWindObject().getInt("speed")+"m/s");
+	     
+		mainLayout.addComponents(dashboardLayout,mainDescriptionLayout, footer);
+	}
+	 
+	
+	 
+
+
 }
